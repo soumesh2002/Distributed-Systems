b859aee54adfd99dfcaa5ef39b000368985a089a
soumesh2002 <soumesh6622@gmail.com>
2022-10-12 21:54:05 +0530

    added print(message) to client

--- 12-10-2022/Chat/Client/client.c
+++ 12-10-2022/Chat/Client/client.c
@@ -11,5 +11,24 @@ int main(int argc, char const *argv[])
     int net_socket = socket(AF_INET, SOCK_STREAM, 0);
     
     struct sockaddr_in serv_addr, cli_addr;
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+    serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
+
+    connect(net_socket, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+
+    char str[200], data[200];
+    int n;
+
+    while (1) {
+        scanf("%s", data);
+        write(net_socket, data, strlen(data));
+        n = recv(net_socket, str, 199, 0);
+        str[n] = '\0';
+        printf("recieved from server: %s\n", str);
+    }
+
+    close(net_socket);
+
     return 0;
 }
